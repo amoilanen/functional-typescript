82f90c51a6b1ad07cee940cadc93559608389dbc
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-04-11 20:40:02 +0300

    Add spec for the monad laws for Option and Reader: function comparison to be fixed

--- spec/typeclasses/monad.spec.ts
+++ spec/typeclasses/monad.spec.ts
@@ -1,10 +1,63 @@
 import { expect } from 'chai';
 
+import { HKT } from '../../src/typeclasses/hkt';
 import { Monad, MonadInstances, ContextDependent_, ContextDependent } from '../../src/typeclasses/monad';
+import { Option } from '../../src/typeclasses/types/option';
 
 describe('monad', () => {
 
-  xdescribe('monad laws', () => {
+  class Eq {
+    static async expectEql<T>(message: string, x: T, y :T): Promise<void> {
+      if (x instanceof Function) {
+        expect(x.toString(), message).to.eql(y.toString());
+      } else if (x instanceof Promise) {
+        expect(await x, message).to.eql(y);
+      } else {
+        expect(x, message).to.eql(y);
+      }
+    }
+  }
+
+  describe('monad laws', () => {
+
+    function checkMonadLaws<M, A, B, C>(m: Monad<M>, a: A, fa: HKT<M, A>, g: (v: A) => HKT<M, B>, h: (v: B) => HKT<M, C>): void {
+      it(`${m.constructor.name} should satisfy monad laws`, async () => {
+        await checkAssociativityLaw(m, fa, g, h);
+        await checkIdentityLaw(m, a, fa, g);
+      });
+    }
+
+    async function checkAssociativityLaw<M, A, B, C>(m: Monad<M>, fa: HKT<M, A>, g: (v: A) => HKT<M, B>, h: (v: B) => HKT<M, C>): Promise<void> {
+      await Eq.expectEql(
+        `associativity law for ${m.constructor.name}`,
+        m.flatMap(m.flatMap(fa, g), h),
+        m.flatMap(fa, x => m.flatMap(g(x), h))
+      );
+    }
+
+    async function checkIdentityLaw<M, A, B>(m: Monad<M>, a: A, fa: HKT<M, A>, g: (v: A) =>HKT<M, B>): Promise<void> {
+      await Eq.expectEql(
+        `left identity law for ${m.constructor.name}`,
+        m.flatMap(m.pure(a), g),
+        g(a)
+      );
+      await Eq.expectEql(
+        `right identity law for ${m.constructor.name}`,
+        m.flatMap(fa, m.pure),
+        fa
+      );
+    }
+
+    checkMonadLaws(MonadInstances.optionMonad, "a", Option.from("a"), s => Option.from(s.length), n => Option.from(`${n}_letters`));
+
+    type StringDependent<T> = ContextDependent<string, T>
+    checkMonadLaws(
+      MonadInstances.readerMonad<string>(),
+      2,
+      ((s: string) => 2) as StringDependent<number>,
+      (n: number) => ((s: string) => Math.abs(n)) as StringDependent<number>,
+      (n: number) => ((s: string) => n > s.length) as StringDependent<boolean>
+    );
   });
 
   describe('Reader monad example: dependency injection', () => {
